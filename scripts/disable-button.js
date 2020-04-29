fde3fe213eb85366f007a599f26fb3e2c5870b3a
MrAdhit <47937950+AdhityaRimba@users.noreply.github.com>
2020-04-29 22:19:09 +0700

    Minified JavaScript

--- scripts/disable-button.js
+++ scripts/disable-button.js
@@ -1,52 +1,5 @@
-var message = "NoRightClicking";
-
-function defeatIE() {
-    if (document.all) {
-        (message);
-        return false;
-    }
-}
-
-function defeatNS(e) {
-    if (document.layers || (document.getElementById && !document.all)) {
-        if (e.which == 2 || e.which == 3) {
-            (message);
-            return false;
-        }
-    }
-}
-if (document.layers) {
-    document.captureEvents(Event.MOUSEDOWN);
-    document.onmousedown = defeatNS;
-} else {
-    document.onmouseup = defeatNS;
-    document.oncontextmenu = defeatIE;
-}
-document.oncontextmenu = new Function("return false")
-
-document.onkeydown = function(e) {
-    if (e.ctrlKey &&
-        (e.keyCode === 67 ||
-            e.keyCode === 86 ||
-            e.keyCode === 85 ||
-            e.keyCode === 117)) {
-        return false;
-    } else {
-        return true;
-    }
-};
-$(document).keypress("u", function(e) {
-    if (e.ctrlKey) {
-        return false;
-    } else {
-        return true;
-    }
-});
-
-$(document).keydown(function(event) {
-    if (event.keyCode == 123) {
-        return false;
-    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {        
-        return false;
-    }
-});
+var message="NoRightClicking";function defeatIE(){if(document.all){(message);return!1}}
+function defeatNS(e){if(document.layers||(document.getElementById&&!document.all)){if(e.which==2||e.which==3){(message);return!1}}}
+if(document.layers){document.captureEvents(Event.MOUSEDOWN);document.onmousedown=defeatNS}else{document.onmouseup=defeatNS;document.oncontextmenu=defeatIE}
+document.oncontextmenu=new Function("return false")
+document.onkeydown=function(e){if(e.ctrlKey&&(e.keyCode===67||e.keyCode===86||e.keyCode===85||e.keyCode===117)){return!1}else{return!0}};$(document).keypress("u",function(e){if(e.ctrlKey){return!1}else{return!0}});$(document).keydown(function(event){if(event.keyCode==123){return!1}else if(event.ctrlKey&&event.shiftKey&&event.keyCode==73){return!1}})
